68069997eb77117f870406e08d64120a02a653dd
Carlos Morales <carlosm3415@gmail.com>
2017-06-09 20:41:14 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 PlatziMusic
+    select(v-model="selectedCountry") 
+      option(v-for="country in countries" v-bind:value="country.value") {{country.name}}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" :artist="artist" :key="artist.mbid")
+</template>
+
+<script>
+import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
+import getArtist from './api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries : [
+        {name: 'Argentina', value:"argentina"},
+        {name: 'Mexico', value:"mexico"},
+        {name: 'España', value:"spain"}
+      ],
+      selectedCountry: 'argentina',
+      loading : true
+    }
+  },
+  methods: {
+    refreshArtist(){
+      const self = this
+      this.loading = true
+      this.artists = []
+      getArtist(this.selectedCountry)
+        .then(function(artists){
+          self.loading = false
+          self.artists = artists
+        })
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  mounted(){
+    this.refreshArtist()
+  },
+  watch:{
+    selectedCountry(){
+      this.refreshArtist()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color red
+    margin-top 60px
+
+h1, h2
+    font-weight normal
+
+ul
+    list-style-type none
+    padding 0
+
+li
+    display inline-block
+    margin 0 10px
+
+a
+    color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default{
+	appName: 'platzimusic',
+	apiKey : '3757051c91ef2929369f31dd4e0cf9b1',
+	secret : '2899e8fb5f4f5885032cd2b4dbce31f1',
+	registeredTo: 'viciotec'
+}	
+
+
+/*Application name	platzimusic
+API key	3757051c91ef2929369f31dd4e0cf9b1
+Shared secret	2899e8fb5f4f5885032cd2b4dbce31f1
+Registered to	viciotec*/
